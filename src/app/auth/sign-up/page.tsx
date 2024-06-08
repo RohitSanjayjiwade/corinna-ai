478d20c986062cdba3780f1873e468ceba3460aa
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-06-08 12:16:42 +0530

    add layout.tsx, install shadcn-ui, update global.css and add images

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+type Props = {
+
+}
+
+const SignUp = async(props:Props)=>{
+	
+	return(<div>SignUp</div>);
+}
+
+export default SignUp;
