06cc23d1b015f009bc82c95d6daf2841a9b6f539
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-06-18 12:37:46 +0530

    create the infobar in settings and add publicRoutes

---
+++ src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,15 @@
+import InfoBar from '@/components/infobar'
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <>
+      <InfoBar />
+      
+    </>
+  )
+}
+
+export default Page
