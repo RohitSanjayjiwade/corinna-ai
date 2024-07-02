65ac4bc861e1e399ee701709eefde3e69b11cabf
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-07-02 12:02:35 +0530

    Bot customization options

--- src/components/forms/settings/form.tsx
+++ src/components/forms/settings/form.tsx
@@ -4,6 +4,8 @@ import { useSettings } from '@/hooks/settings/use-settings'
 import React from 'react'
 import { DomainUpdate } from './domain-update'
 import CodeSnippet from './code-snippet'
+import PremiumBadge from '@/icons/premium-badge'
+import EditChatbotIcon from './edit-chatbot-icon'
 
 type Props = {
   id: string
@@ -39,6 +41,25 @@ const SettingsForm = ({ id, name, chatBot, plan }: Props) => {
           errors={errors}
         />
         <CodeSnippet id={id} />
+      </div>
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex gap-4 items-center">
+          <h2 className="font-bold text-2xl">Chatbot Settings</h2>
+          <div className="flex gap-1 bg-cream rounded-full px-3 py-1 text-xs items-center font-bold">
+            <PremiumBadge />
+            Premium
+          </div>
+        </div>
+        <Separator orientation="horizontal" />
+        <div className="grid md:grid-cols-2">
+           <div className="col-span-1 flex flex-col gap-5 order-last md:order-first">
+             <EditChatbotIcon
+              chatBot={chatBot}
+              register={register}
+              errors={errors}
+            />
+           </div>
+        </div>
       </div>
     </form>
   )
