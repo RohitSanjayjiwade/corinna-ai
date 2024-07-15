39d86ff22c9430fbe456035a57696052295b424a
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-07-15 13:49:18 +0530

    How to use stripe connect in nextjs14 application to sell our customers products

---
+++ src/constants/integrations.ts
@@ -0,0 +1,21 @@
+type IntegrationsListItemProps = {
+  id: string
+  name: 'stripe'
+  logo: string
+  description: string
+  title: string
+  modalDescription: string
+}
+
+export const INTEGRATION_LIST_ITEMS: IntegrationsListItemProps[] = [
+  {
+    id: '1',
+    name: 'stripe',
+    description:
+      'Stripe is the fastest and easiest way to integrate payments and financial services into your software platform or marketplace.',
+    logo: '5315ec8b-26e7-4133-aec5-cea549cff0fc',
+    title: 'Connect Stripe Account',
+    modalDescription:
+      'The world’s most successful platforms and marketplaces including Shopify and DoorDash, use Stripe Connect.',
+  },
+]
