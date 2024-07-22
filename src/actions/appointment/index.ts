7ba6ac3a8be5cf11bfe55e98953133fa88d9c3c4
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-07-22 16:03:27 +0530

    Build a Dashboard in Nextjs15

--- src/actions/appointment/index.ts
+++ src/actions/appointment/index.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { client } from '@/lib/prisma'
+import { currentUser } from '@clerk/nextjs'
 
 export const onDomainCustomerResponses = async (customerId: string) => {
   try {
@@ -148,6 +149,32 @@ export const onGetAllBookingsForCurrentUser = async (clerkId: string) => {
     if (bookings) {
       return {
         bookings,
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+export const getUserAppointments = async () => {
+  try {
+    const user = await currentUser()
+    if (user) {
+      const bookings = await client.bookings.count({
+        where: {
+          Customer: {
+            Domain: {
+              User: {
+                clerkId: user.id,
+              },
+            },
+          },
+        },
+      })
+
+      if (bookings) {
+        return bookings
       }
     }
   } catch (error) {
